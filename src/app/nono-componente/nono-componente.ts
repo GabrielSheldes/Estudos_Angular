d948e4178d7b98393a01c4e9178eb63331b2f538
GabrielSheldes <gabriel.arantes9@etec.sp.gov.br>
2025-09-09 22:14:23 -0300

    formulario

--- src/app/nono-componente/nono-componente.ts
+++ src/app/nono-componente/nono-componente.ts
@@ -1,17 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-nono-componente',
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, FormGroup, ReactiveFormsModule,FormControl],
   templateUrl: './nono-componente.html',
   styleUrl: './nono-componente.css'
 })
 export class NonoComponente {
-//Inclusão de duas variaves 
-  nome: string;
-  cidade: string;
-  idade: number= null;
-
+//Objeto de formulario 
+  formulario = new FormGroup({
+  nome: new FormControl ('',[Validators.required, Validators.minLength(3)]),
+  cidade: new FormControl('',[Validators.required, Validators.minLength(3)]),
+  idade: new FormControl(null, [Validators.required, Validators.min(0), Validators.max(120)])
+});
 }
