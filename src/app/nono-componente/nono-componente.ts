712ae24f4c129a81cb2a6b4ec40890705d11dd19
GabrielSheldes <gabriel.arantes9@etec.sp.gov.br>
2025-09-23 20:34:20 -0300

    nono-componente

--- src/app/nono-componente/nono-componente.ts
+++ src/app/nono-componente/nono-componente.ts
@@ -15,7 +15,8 @@ export class NonoComponente {
   formulario = new FormGroup({
   nome: new FormControl ('',[Validators.required, Validators.minLength(3)]),
   cidade: new FormControl('',[Validators.required, Validators.minLength(3)]),
-  idade: new FormControl(null, [Validators.required, Validators.min(0), Validators.max(120)])
+  idade: new FormControl(null, [Validators.required, Validators.min(0), Validators.max(120)]),
+  sexo: new FormControl('M',[Validators.required])
 });
 
 btnCadastrar: boolean = true;
@@ -40,10 +41,42 @@ selecionar(indice: number){
     this.formulario.setValue({
       nome: this.vetor[indice].nome,
       idade: this.vetor[indice].idade,
-      cidade: this.vetor[indice].cidade
+      cidade: this.vetor[indice].cidade,
+      sexo: this.vetor[indice].sexo
     });
     // alterar o estado do botao
     this.btnCadastrar = false;
-
   } 
+//Função de alteração
+alterar(){
+  this.vetor[this.indice] = this.formulario.value as Pessoa;
+
+  //limpar os imputs
+  this.formulario.reset();
+
+  //Visibilidades de botões
+  this.btnCadastrar = true;
+}
+remover(){
+  //removendo pessoa do vetor 
+  this.vetor.splice(this.indice, 1);
+
+  //limpeza dos imputs
+  this.formulario.reset();
+
+  //visibilidade dos botões
+  this.btnCadastrar = true;
+
+}
+
+  //botão de cancelamento
+cancelar(){
+
+  //limpeza dos imputs
+  this.formulario.reset();
+
+  //visibilidade dos botões
+  this.btnCadastrar = true;
+
+  };
 }
